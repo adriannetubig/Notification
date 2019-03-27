95f0514f16d5e3429780830e166c6b1b90eecec2
adriannetubig <adriannetubig@gmail.com>
2019-03-27 11:09:27 +0800

    AuthenticationWeb

---
+++ AuthenticationWeb/Controllers/AuthenticationsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using AuthenticationWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationsController : ControllerBase
+    {
+        private IConfiguration Configuration { get; }
+        public AuthenticationsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        [AllowAnonymous, HttpPost("NoAuthorization")]
+        public IActionResult NoAuthorization()
+        {
+            return Ok("NoAuthorization");
+        }
+
+        [Authorize, HttpPost("WithAuthorization")]
+        public IActionResult WithAuthorization()
+        {
+            return Ok(User.Identities.FirstOrDefault().Name);
+        }
+
+        [AllowAnonymous ,HttpPost, Route("Login")]
+        public IActionResult Login([FromBody]LoginModel user)
+        {
+            //ToDo: put this in Business Logic
+            //ToDo: do not use static username and password
+            if (user == null && (user.UserName != "username" && user.Password != "password"))
+                return Unauthorized();
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("Authentication:IssuerSigningKey").Value));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Role, "Manager")
+            };
+
+            var tokeOptions = new JwtSecurityToken(
+                issuer: Configuration.GetSection("Authentication:ValidIssuer").Value,
+                audience: Configuration.GetSection("Authentication:ValidAudience").Value,
+                claims: claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(Configuration.GetSection("Authentication:ExpiresMinutes").Value)),
+                signingCredentials: signinCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return Ok(new { Token = tokenString });
+        }
+    }
+}
---
+++ AuthenticationWeb/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticationWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddNewtonsoftJson();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = Configuration.GetSection("Authentication:ValidIssuer").Value,
+                    ValidAudience = Configuration.GetSection("Authentication:ValidAudience").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetSection("Authentication:IssuerSigningKey").Value)),
+                    ClockSkew = TimeSpan.FromMinutes(Convert.ToDouble(Configuration.GetSection("Authentication:ClockSkewMinutes").Value))
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting(routes =>
+            {
+                routes.MapControllers();
+            });
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
