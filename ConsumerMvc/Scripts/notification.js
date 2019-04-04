15f669a79dc61203c0dde933db1be8297d046be5
adriannetubig <adriannetubig@gmail.com>
2019-04-04 09:56:03 +0800

    Used npm for client side 3rd party library

--- ConsumerMvc/Scripts/notification.js
+++ ConsumerMvc/Scripts/notification.js
@@ -1,23 +1,17 @@
 ﻿"use strict";
 
-//var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:40902/notificationHub", {
-//    accessTokenFactory: () =>
-//        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiam9obmRvZSIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6Ik1hbmFnZXIiLCJuYmYiOjE1NTM3NDE2MTUsImV4cCI6MTU1Mzc0MTY3NSwiaXNzIjoiSXNzdWVyIiwiYXVkIjoiQXVkaWVuY2UifQ.2HBSiii7sJAXg7dC55e5cKIUU1-TxYEAQphqzw0s85w"
-//})
-//    .build();
-//var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:40902/notificationHub").build();
-var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:40902/authenticatedHub").build();
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:40902/notificationHub").build();
 
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-//connection.on("ReceiveMessage", function (user, message) {
-//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//    var encodedMsg = user + " says " + msg;
-//    var li = document.createElement("li");
-//    li.textContent = encodedMsg;
-//    document.getElementById("messagesList").appendChild(li);
-//});
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
 
 connection.on("AuthorizedMessage", function (notification) {
     var encodedMsg = notification.eventDate + " " + notification.sender + " : " + notification.message;
@@ -35,11 +29,8 @@ connection.start().then(function () {
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
-    connection.invoke("WithAuthorization", user, message).catch(function (err) {
+    connection.invoke("WithoutAuthorization", user, message).catch(function (err) {
         return console.error(err.toString());
     });
-    //connection.invoke("WithoutAuthorization", user, message).catch(function (err) {
-    //    return console.error(err.toString());
-    //});
     event.preventDefault();
 });
--- SignalRWeb/Hubs/NotificationHub.cs
+++ SignalRWeb/Hubs/NotificationHub.cs
@@ -1,21 +1,13 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace SignalRWeb.Hubs
 {
-    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class NotificationHub : Hub
     {
-        //public async Task WithoutAuthorization(string user, string message)
-        //{
-        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-        //}
-
-        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        //public async Task WithAuthorization(string user, string message)
-        //{
-        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-        //}
+        public async Task WithoutAuthorization(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
     }
 }
