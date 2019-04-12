200b2cf3b5daa153e6e378218a3f8fa7b7d7e8f1
adriannetubig <adriannetubig@gmail.com>
2019-04-12 14:48:59 +0800

    Updated event date. Event Date is wrong when inherited.

--- SignalRModel/Notification.cs
+++ SignalRModel/Notification.cs
@@ -4,7 +4,21 @@ namespace SignalRModel
 {
     public class Notification
     {
-        public DateTime EventDate => DateTime.Now;
+        private DateTime? _eventDate;
+        public DateTime EventDate
+        {
+            get
+            {
+                if (_eventDate.HasValue)
+                    return _eventDate.Value;
+                else
+                    return DateTime.UtcNow;
+            }
+            set
+            {
+                _eventDate = value;
+            }
+        }
         public string Sender { get; set; }
         public string Message { get; set; }
     }
